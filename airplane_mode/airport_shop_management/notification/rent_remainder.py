04329b442ec98b711a5186ae564b47db8cdc5f8d
sivamani <siva.mv@promantia.com>
2024-07-04 16:58:43 +0530

    changes deployed

--- airplane_mode/airport_shop_management/notification/rent_remainder.py
+++ airplane_mode/airport_shop_management/notification/rent_remainder.py
@@ -1,25 +1,16 @@
-# tasks.py
-
 import frappe
-from frappe.utils import get_url, nowdate
-
 
 @frappe.whitelist()
 def send_rent_reminder_emails():
-    tenants = frappe.db.sql(
-        """ select * from `tabTenant Contract`""", as_dict=True)
+    tenants = frappe.db.sql("""SELECT * FROM `tabTenant Contract`""", as_dict=True)
     for tenant in tenants:
         tenant_doc = frappe.get_doc("Tenant", tenant.tenant)
-        send_rent_reminder_email(
-            tenant_doc.tenant_name, tenant_doc.email, tenant.payment_due_date)
-
+        send_rent_reminder_email(tenant_doc.tenant_name, tenant_doc.email, tenant.payment_due_date)
 
 def send_rent_reminder_email(user_name, email, due_date):
     subject = "Monthly Rent Due Reminder"
-    message = f"""
-    <p>Dear {user_name},</p>
-    <p>This is a reminder that your rent is due on {due_date}. Please make the payment by the end of the month to avoid any late fees.</p>
-    <p>Thank you.</p>
-    """
+    message = f"""<p>Dear {user_name},</p>
+                <p>This is a reminder that your rent is due on {due_date}. Please make the payment by the end of the month to avoid any late fees.</p>
+                <p>Thank you.</p>"""
     recipients = email
     frappe.sendmail(recipients=recipients, subject=subject, message=message)
